7f144ea60c54e85b5a5f02386b4bae2f4ae2e1f4
Rabia <coder.zone1111@gmail.com>
2025-11-09 11:00:56 +0500

    Add Mad Libs Python game code

---
+++ MadLibsGame.py
@@ -0,0 +1,8 @@
+print ('welcome to Mad Libs')
+name = input('Enter a name')
+place = input('Enter a place')
+adjective =input('Enter an adjective')
+noun = input('Enter a noun')
+
+story = f"{name} went to {place} and found a {adjective} {noun}"
+print(story)
